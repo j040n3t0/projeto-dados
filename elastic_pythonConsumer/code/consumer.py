e71012232e04e8b0474d16d8255a0bc75447d47b
j040n3t0 <joaojose.ti@gmail.com>
2021-08-19 16:06:02 -0400

    Atualizado etapa do consumidor Python e do Elasticsearch

---
+++ elastic_pythonConsumer/code/consumer.py
@@ -0,0 +1,85 @@
+# Importa as bibliotecas necessarias
+from kafka import KafkaConsumer
+from datetime import date, datetime
+from json import loads
+import os, time
+
+# Cria o consumer
+def setKafkaConnection():
+    global consumer
+    try: 
+        consumer = KafkaConsumer(
+        # Informa o topico
+            'fullfillment.public.inventory', 
+            # Informa o servidor Kafka
+            bootstrap_servers='192.168.1.136:9094',
+            # Informa a qual grupo o consumer pertence
+            group_id='python-consumer'
+        )
+        return 'success'
+    except:
+        return 'false'
+
+# Funcao para enviar alerta, recebe o message do offset do Kafka para envio
+def sendAlert(message):
+    # Token do Bot Telegram responsavel por enviar a mensagem
+    bottoken = "421548220:AAFAtwLxRUzKae9b50UONZhq0S9OIjhxSeg"
+    # O ID do Chat para o qual o Bot devera enviar a mensagem
+    chatid = "93702435"
+
+    # Envia a mensagem utilizando CURL e os dados acima informados.
+    os.system("curl -s -X POST \
+        -H 'Content-Type: application/json' \
+        -d '{\"chat_id\": %s, \"text\": \"%s\", \"disable_notification\": true}' \
+        https://api.telegram.org/bot%s/sendMessage >> /dev/null" % (chatid,message,bottoken))
+
+# Variavel de controle para inicializacao
+count = 1
+# Cria laco infinito para ficar processamento as mensagem do topico do Kafka
+while True:
+    try:
+        # Checa se e a primeira conexao
+        if count == 1:
+            try:
+                print("Testando primeira conexao com Kafka!!")
+                # Chama funcao de conexao com Kafka
+                status = setKafkaConnection()
+                # Estrutura condicional para validar o restante do script
+                if status == "success":
+                    print("Conexao estabelecida com sucesso!\n\nCarregando mensagem do Kafka...\n")
+                else:
+                    print("Deu ruim!! Vamos continuar tentando conectar...\n\n")
+            except:
+                pass
+
+        # Entra no objeto consumer para ler as mensagens
+        for message in consumer:
+            # Guarda o timestamp do evento do Kafka
+            timestamp = datetime.fromtimestamp(message.timestamp/1000.0).strftime("%A, %B %d, %Y %H:%M:%S")
+            # Guarda a mensagem do evento do Kafka
+            message = message.value.decode("utf-8")
+            # Imprime a saida horario + menssagem do evento
+            print ("%s: %s" % (timestamp,message))
+            # Envia alerta via Telegram
+            #sendAlert(message.value.decode("utf-8"))
+
+    # Mensagem ao apertar CTRL+C para finalizar o script
+    except KeyboardInterrupt:
+        print("\n[*]CTRL+C was pressed...")
+        # Quebra o Loop 'while True'
+        break
+    except:
+        # Mensagem caso o script nao conecte com o Kafka
+        print("DIED! You need to investigate the reason!!")
+        # Tempo de espera para nova execucao
+        time.sleep(count)
+        # Incrementa o tempo de execucao com +1 segundo
+        count = count + 1
+        # Apos o tempo de espera o script tenta conectar novamente no Kafka
+        try:
+            status = setKafkaConnection()           
+            # Estrutura condicional para validar a conexao com o Kafka esta Ok
+            if status == "success":
+                print("[*]Estamos de volta Baby! ;)\n\nCarregando mensagem do Kafka...\n")          
+        except:
+            pass
---
+++ elastic_pythonConsumer/code/consumer_kafka.py
@@ -0,0 +1,105 @@
+# Importa as bibliotecas necessarias
+from kafka import KafkaConsumer
+from datetime import date, datetime
+from json import loads
+import os, time, json, requests
+
+def writeLog(log):
+    with open('somefile.txt', 'a') as the_file:
+        the_file.write(log+'\n')
+
+def sendElastic(kafkaData):
+    writeLog("Entrou na funcao de envia de logs para Elastic")
+    url = "http://192.168.1.136:9200/pessoa_kafka/_doc/"+str(kafkaData['id'])
+
+    payload = json.dumps(kafkaData)
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.status_code
+    
+# Cria o consumer
+def setKafkaConnection():
+    writeLog("Entrou na funcao de conexao com Kafka!!")
+    global consumer
+    try: 
+        consumer = KafkaConsumer(
+        # Informa o topico
+            'fullfillment.public.inventory', 
+            # Informa o servidor Kafka
+            bootstrap_servers='host.docker.internal:9094',
+            # Informa a qual grupo o consumer pertence
+            group_id='python-consumer'
+        )
+        # writeLog(str(consumer))
+        writeLog("Sucesso!!")
+        return 'success'
+    except:
+        writeLog("Erro!!")
+        return 'false'
+
+# Variavel de controle para inicializacao
+count = 1
+# Cria laco infinito para ficar processamento as mensagem do topico do Kafka
+while True:
+    try:
+        # Checa se e a primeira conexao
+        if count == 1:
+            try:
+                writeLog("Testando primeira conexao com Kafka!!")
+                # Chama funcao de conexao com Kafka
+                status = setKafkaConnection()
+                writeLog(status)
+                # Estrutura condicional para validar o restante do script
+                if status == "success":
+                    writeLog("Conexao estabelecida com sucesso!\n\nCarregando mensagem do Kafka...\n")
+                else:
+                    writeLog("Deu ruim!! Vamos continuar tentando conectar...\n\n")
+            except:
+                pass
+
+        writeLog("Ultimo passo antes do loop!!")
+
+        # Entra no objeto consumer para ler as mensagens
+        for message in consumer:
+            writeLog("Entrou no loop de leitura de mensagens!!")
+            # Guarda o timestamp do evento do Kafka
+            timestamp = datetime.fromtimestamp(message.timestamp/1000.0).strftime("%A, %B %d, %Y %H:%M:%S")
+            # Guarda a mensagem do evento do Kafka
+            json_message = message.value.decode("utf-8")
+            #print(type(json_message))
+            messagem = json.loads(json_message)
+            clean_message = messagem['payload']['after']
+            # Imprime a saida horario + menssagem do evento
+            writeLog ("%s: %s" % (timestamp,json.dumps(clean_message)))
+            status = sendElastic(clean_message)
+            if(status == 200 or status == 201):
+                writeLog("[*] Dado inserido no Elasticsearch com sucesso!")
+            else:
+                writeLog("[!] Falha ao inserir dado no Elasticsearch!! X.X")
+            # Envia alerta via Telegram
+            #sendAlert(message.value.decode("utf-8"))
+
+    # Mensagem ao apertar CTRL+C para finalizar o script
+    except KeyboardInterrupt:
+        writeLog("\n[*]CTRL+C was pressed...")
+        # Quebra o Loop 'while True'
+        break
+    except:
+        # Mensagem caso o script nao conecte com o Kafka
+        writeLog("DIED! You need to investigate the reason!!")
+        # Tempo de espera para nova execucao
+        time.sleep(count)
+        # Incrementa o tempo de execucao com +1 segundo
+        count = count + 1
+        # Apos o tempo de espera o script tenta conectar novamente no Kafka
+        try:
+            status = setKafkaConnection()           
+            # Estrutura condicional para validar a conexao com o Kafka esta Ok
+            if status == "success":
+                writeLog("[*]Estamos de volta Baby! ;)\n\nCarregando mensagem do Kafka...\n")          
+        except:
+            pass
